b5d774b78c7d4e2541017805e0107a3c7a4dc556
Curtis <cutwen22s@gmail.com>
2018-02-20 14:52:25 -0800

    For my branch

--- SDES/functions.cpp
+++ SDES/functions.cpp
@@ -5,12 +5,28 @@
 
 using namespace std;
 
+int S0[4][4] = { { 1,0,3,2 },
+{ 3,2,1,0 },
+{ 0,2,1,3 },
+{ 3,1,3,2 } };
+
+int S1[4][4] = { { 0,1,2,3 },
+{ 2,0,1,3 },
+{ 3,0,1,0 },
+{ 2,1,0,3 } };
+
 bool* getInput(int textType)
 {
-	auto size = 8;
-	if (textType == key) size = 10;
+	int size;
+	if (textType == key)
+		size = 10;
+	else
+		size = 8;
 
-	auto* const inputText = new bool[size];
+	bool* inputText = new bool[size];
+	
+
+
 
 	for(int i{}; i < size; i++)
 	{
@@ -26,7 +42,7 @@ bool* getInput(int textType)
 
 bool* IPFunction(bool* text)
 {
-	auto const tempText = new bool[8];
+	bool tempText[8];
 	tempText[0] = text[1];
 	tempText[1] = text[5];
 	tempText[2] = text[2];
@@ -36,14 +52,13 @@ bool* IPFunction(bool* text)
 	tempText[6] = text[4];
 	tempText[7] = text[6];
 
-	delete[] text;
-	text = tempText;
-	return text;
+	
+	return tempText;
 }
 
 bool* InverseIPFunction(bool* text)
 {
-	auto const tempText = new bool[8];
+	bool tempText[8];
 	tempText[0] = text[3];
 	tempText[1] = text[0];
 	tempText[2] = text[2];
@@ -53,34 +68,46 @@ bool* InverseIPFunction(bool* text)
 	tempText[6] = text[7];
 	tempText[7] = text[5];
 
-	delete[] text;
-	text = tempText;
-	return text;
+	return tempText;
 }
 
 bool* FunctionFk(bool* text, bool* subkey1)
 {
 	// E/P Expansion permutation
-	auto const tempText = new bool[8];
-	tempText[0] = text[3] ^ subkey1[0];
-	tempText[1] = text[0] ^ subkey1[1];
-	tempText[2] = text[1] ^ subkey1[2];
-	tempText[3] = text[2] ^ subkey1[3];
-	tempText[4] = text[1] ^ subkey1[4];
-	tempText[5] = text[2] ^ subkey1[5];
-	tempText[6] = text[4] ^ subkey1[6];
-	tempText[7] = text[1] ^ subkey1[7];
+	bool eP[8]; //vlaues for the text being permutated
+	int p4Decimal[2]; //decimal values returned from the tables
+	bool p4Binary[4]; //binary values from the decimal values
+	bool tR[4]; //table result container (from positions in s0 and s1)
 
-	//need to input this into S0 and S1
+	eP[0] = text[3] ^ subkey1[0];
+	eP[1] = text[0] ^ subkey1[1];
+	eP[2] = text[1] ^ subkey1[2];
+	eP[3] = text[2] ^ subkey1[3];
 
-	delete[] text;
-	text = tempText;
-	return text;
+	eP[4] = text[1] ^ subkey1[4];
+	eP[5] = text[2] ^ subkey1[5];
+	eP[6] = text[3] ^ subkey1[6];
+	eP[7] = text[0] ^ subkey1[7];
+
+
+	//get values from table
+	p4Decimal[0] = S0[binaryConversion(eP[0], eP[3])][binaryConversion(eP[1], eP[2])];
+	p4Decimal[1] = S1[binaryConversion(eP[4], eP[7])][binaryConversion(eP[5], eP[6])];
+
+	bool* p4Holder1 = decimalConversion(p4Decimal[0]);
+	bool* p4Holder2 = decimalConversion(p4Decimal[2]);
+
+	tR[0] = p4Holder1[0];
+	tR[1] = p4Holder1[1];
+	tR[2] = p4Holder2[0];
+	tR[3] = p4Holder2[1];
+
+	return tR;
 }
 
 bool* Switch(bool* text)//Switches the first and second halves of 8 bit data.
 {
-	auto const tempText = new bool[8];
+	bool tempText[8];
 	tempText[0] = text[4];
 	tempText[1] = text[5];
 	tempText[2] = text[6];
@@ -90,46 +117,13 @@ bool* Switch(bool* text)//Switches the first and second halves of 8 bit data.
 	tempText[6] = text[2];
 	tempText[7] = text[3];
 
-	delete[] text;
-	text = tempText;
-	return text;
+	return tempText;
 }
 
 bool* GenKey1(bool* text)//[10]
 {
-	/*auto const keypart1 = new bool[5];
-	auto const keypart2 = new bool[5];
-	keypart1[0] = text[2];
-	keypart1[1] = text[4];
-	keypart1[2] = text[1];
-	keypart1[3] = text[6];
-	keypart1[4] = text[3];
-
-	keypart2[0] = text[9];
-	keypart2[1] = text[0];
-	keypart2[2] = text[8];
-	keypart2[3] = text[7];
-	keypart2[4] = text[5];
-
-	auto const newpart1 = new bool[5];
-	auto const newpart2 = new bool[5];
-	auto size = sizeof(keypart1)/sizeof(keypart1[0]);
-	memcpy_s(newpart1, size, keypart1, size);
-	memcpy_s(newpart2, size, keypart2, size);
-
-	newpart1[0] = newpart1[3];
-	newpart1[1] = newpart1[4];
-	newpart1[2] = newpart1[0];
-	newpart1[3] = newpart1[1];
-	newpart1[4] = newpart1[2];
-
-	newpart2[0] = newpart2[3];
-	newpart2[1] = newpart2[4];
-	newpart2[2] = newpart2[0];
-	newpart2[3] = newpart2[1];
-	newpart2[4] = newpart2[2];*/
-
-	auto const key1 = new bool[8];
+	
+	bool key1[8];
 	key1[0] = text[5];
 	key1[1] = text[4];
 	key1[2] = text[9];
@@ -139,14 +133,12 @@ bool* GenKey1(bool* text)//[10]
 	key1[6] = text[7];
 	key1[7] = text[8];
 
-	delete[] text;
-	text = key1;
-	return text;
+	return key1;
 }
 
 bool* GenKey2(bool* text)//[10]
 {
-	auto const key2 = new bool[8];
+	bool key2[8];
 	key2[0] = text[7];
 	key2[1] = text[2];
 	key2[2] = text[5];
@@ -156,7 +148,48 @@ bool* GenKey2(bool* text)//[10]
 	key2[6] = text[8];
 	key2[7] = text[0];
 
-	delete[] text;
-	text = key2;
-	return text;
+	return key2;
+}
+
+
+int binaryConversion(bool one, bool two)
+{
+
+	if (one && two)
+		return 3;
+	else if (one && !two)
+		return 2;
+	else if (!one && two)
+		return 1;
+	else
+		return 0;
+
+}
+
+bool* decimalConversion(int decimal)
+{
+	bool bits[2];
+
+	if (decimal = 3)
+	{
+		bits[0] = 1;
+		bits[1] = 1;
+	} else if (decimal = 2)
+	{
+		bits[0] = 1;
+		bits[1] = 0;
+	}
+	else if (decimal = 1)
+	{
+		bits[0] = 0;
+		bits[1] = 1;
+	}
+	else
+	{
+		bits[0] = 0;
+		bits[1] = 0;
+	}
+		
+	return bits;
+
 }
